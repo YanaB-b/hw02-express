e8feff698f0107b314b7be68e77d1c5db686a45f
YanaB-b <j.biliuk96@gmail.com>
2023-06-18 21:36:58 +0200

    +

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const ctrl = require("../../controllers/contacts");
 const router = express.Router();
-const { validateBody } = require("../../middlewares");
+const { validateBody, isValidId } = require("../../middlewares");
 const schemas = require("../../schemas/contacts");
 
 router.get("/", ctrl.listContacts);
 
-router.get("/:id", ctrl.getContactById);
+router.get("/:id", isValidId, ctrl.getContactById);
 
 router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
 
-router.delete("/:id", ctrl.removeContact);
+router.delete("/:id", isValidId, ctrl.removeContact);
 
-router.put("/:id", ctrl.updateContact);
+router.put("/:id", isValidId, ctrl.updateContact);
+
+router.patch("/:id/favorite", isValidId, ctrl.updateFavorite);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const app = require('./app')
-const mongoose = require('mongoose');
-const {DB_HOST}= process.env;
+const app = require("./app");
+const mongoose = require("mongoose");
+const { DB_HOST, PORT = 3000 } = process.env;
 
-mongoose.set('strictQuery', true);
-mongoose.connect(DB_HOST)
-.then(()=>{
-  app.listen(3000, () => {
-    console.log("Server running. Use our API on port: 3000")
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+    console.log("Database connection successful");
   })
-})
-.catch(error=>{
-  console.log(error.message);
-  process.exit(1);
-})
-
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
