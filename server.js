96a807d816cd9726fed23e2702c20f2717f68bc1
YanaB-b <j.biliuk96@gmail.com>
2023-06-18 11:05:27 +0200

    init project

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const app = require('./app')
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const DB_HOST = 'mongodb+srv://Yana:XgfsM3MFN6sN75F7@cluster0.5i6g5la.mongodb.net/myContacts?retryWrites=true&w=majority'
+mongoose.set('strictQuery', true);
+mongoose.connect(DB_HOST)
+.then(()=>{
+  app.listen(3000, () => {
+    console.log("Server running. Use our API on port: 3000")
+  })
 })
+.catch(error=>{
+  console.log(error.message);
+  process.exit(1);
+})
+
